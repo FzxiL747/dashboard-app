baa63d06a8b1224ac6a3b9dbec15680ff50dd992
FzxiL747 <ssyedfuzail@gmail.com>
2026-03-28 23:41:56 +0530

    initial commit

---
+++ dashboard-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>🚀 My Dashboard</h1>
+      <p>Clean start. No bugs this time.</p>
+    </div>
+  );
+}
+
+export default App;
